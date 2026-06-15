9bce69ef0ec124e3d614c03bbe6031f243a8cf22
shivam ojha <143830066+shivam6677ojh@users.noreply.github.com>
2026-06-15 08:01:48 +0530

    adding files and setup for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,9 +15,19 @@ app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 
+
+
+
 if (env.NODE_ENV !== "test") {
   app.use(morgan(env.NODE_ENV === "production" ? "combined" : "dev"));
 }
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "ok",
+    message: "Spreetail Backend API is running",
+  });
+});
 
 app.use("/api", apiRouter);
 app.use(notFound);
